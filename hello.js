569c8506179c22f7f31ae17b2e52f58c8c53dd67
Vinay Gonela <74185799+VinayGonela@users.noreply.github.com>
2020-12-27 12:58:07 +0530

    added js file

---
+++ hello.js
@@ -0,0 +1,17 @@
+// Load HTTP module
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 8000;
+
+// Create HTTP server
+const server = http.createServer((req, res) => {
+
+ // Send the response body "Hello World"
+   res.end('Hello World\n');
+});
+
+// Prints a log once the server starts listening
+server.listen(port, hostname, () => {
+   console.log(`Server running at http://${hostname}:${port}/`);
+})
